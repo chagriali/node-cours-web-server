5147e9901d3b8fb637cb447e396b721c988a0766
Ali Chagri <Ali Chagri>
2017-04-04 12:20:24 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getYear',()=>{
+  return new Date().getFullYear();
+});
+
+app.use((req,res,next) => {
+    console.log(req.method);
+    next();
+});
+
+app.use((req,res,next) => {
+  res.render('maintenance.hbs')
+});
+
+
+app.get('/',(req,res) => {
+  res.render('home.hbs',{
+    Title: 'Home Page',
+    welcome: 'Welcome to our site'
+  })
+});
+
+
+
+
+app.get('/bad',(req, res) => {
+  res.send({
+    errorMessage : 'Error'
+  })
+})
+
+
+app.listen(3005);
